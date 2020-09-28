c0649b486cb7a31297b9f9701ba0978bdc7497f1
不掉头发的程序猿 <71914638+lzj1289140507@users.noreply.github.com>
2020-09-28 10:52:17 +0800

    导航栏
    添加了导航栏，划分了模块

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,54 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+    <a class="navbar-brand" href="/">首页</a>
+    <button
+      class="navbar-toggler d-lg-none"
+      type="button"
+      data-toggle="collapse"
+      data-target="#collapsibleNavId"
+      aria-controls="collapsibleNavId"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavId">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="/about">
+            单选题
+          </a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/bbout">
+            多选题
+          </a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="/cbout">
+            历史结果
+          </a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input
+          class="form-control mr-sm-2"
+          type="text"
+          placeholder="搜索题目、用户"
+        />
+        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">
+          搜索
+        </button>
+      </form>
     </div>
+  </nav>
     <router-view/>
   </div>
 </template>
 
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
