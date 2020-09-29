7c82b5db22df4a9817eaa8f0d9e7d7c5caf9b424
不掉头发的程序猿 <71914638+lzj1289140507@users.noreply.github.com>
2020-09-29 11:50:05 +0800

    错题集

--- src/components/MyNav.vue
+++ src/components/MyNav.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-      <a class="navbar-brand" href="/">首页</a>
+      <router-link class="nav-link" to="/">首页</router-link>
       <button
         class="navbar-toggler d-lg-none"
         type="button"
@@ -11,18 +11,17 @@
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="collapsibleNavId">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item active">
-            <a class="nav-link" href="/cbout"> 历史记录 </a>
+            <router-link class="nav-link" to="/bbout">错题集</router-link>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="/bbout"> 错题集 </a>
+            <router-link class="nav-link" to="/cbout">历史记录</router-link>
           </li>
-           <li class="nav-item active">
-            <a class="nav-link" href="/about"> 扩展题库 </a>
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/about">扩展题库</router-link>
           </li>
         </ul>
       </div>
--- src/views/Bbout.vue
+++ src/views/Bbout.vue
@@ -1,22 +1,43 @@
 <template>
-    <div class="bbout">
-        <MyNav />
-         <div class="jumbotron">
-            <h1 class="display-3">错题集</h1>
-            <span class="badge badge-pill badge-success">这是所有的错题了</span>
-        </div>
+  <div class="bbout">
+    <MyNav />
+    <div class="jumbotron">
+      <h1 class="display-3">错题集</h1>
+      <span class="badge badge-pill badge-success">错题数目：{{cnu.length}}</span>
     </div>
+
+    <div class="container">
+      <div class="jumbotron" v-for="(item, index) in cnu" :key="index">
+          <h1 class="display-3">{{item.title}}</h1>
+          <h3 class="lead" v-for="(item, index) in item.ans" :key="index">
+            <span class="badge badge-pill badge-success">{{
+              String(Object.keys(item))
+            }}</span>
+            {{ String(Object.values(item)) }}
+          </h3>
+          <h3>正确答案是：{{ item.right }}</h3>
+          <p>
+            您选的答案是:<span class="badge badge-pill badge-danger">{{
+              item.cright}}</span>
+          </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import MyNav from "../components/MyNav.vue";
+import { mapState } from "vuex";
 
 export default {
-     components: {
-     MyNav,
+  components: {
+    MyNav,
   },
-}
+  name: "Bbout",
+  computed: {
+    ...mapState(["cnu", "now"]),
+  },
+};
 </script>
 <style lang="css">
-    
 </style>
